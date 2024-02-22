0a043caf5e1e398f3a7687eb6f05e6200b071df6
AlphaFoxz <841958335@qq.com>
2024-02-22 23:01:07 +0800

    wip

--- src/main/java/com/github/alphafoxz/oneboot/core/standard/service/OnebootMapper.java
+++ src/main/java/com/github/alphafoxz/oneboot/core/standard/service/OnebootMapper.java
@@ -3,14 +3,12 @@ package com.github.alphafoxz.oneboot.core.standard.service;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  *
  */
+@Inherited
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.CLASS)
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
